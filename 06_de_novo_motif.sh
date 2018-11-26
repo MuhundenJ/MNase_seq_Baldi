2f08f3115acf1a218f15ec4c4c7907f319fa679d
bmc-CompBio <tobias.straub@lmu.de>
2018-11-26 09:38:58 +0100

    small fixes

--- 06_de_novo_motif.sh
+++ 06_de_novo_motif.sh
@@ -7,10 +7,10 @@ module load meme/5.0.2
 module load ngs/Homer/4.10
 
 # all MEME
-#meme seqs/downPNA.fasta -nmotifs 10 -dna -revcomp -mod zoops -oc motifs/meme_zoops -p 62
+meme seqs/downPNA.fasta -nmotifs 10 -dna -revcomp -mod zoops -oc motifs/meme_zoops -p 62
 fimo --oc hits/meme_zoops_m1 --motif BGGMCMACRBKGCGTATRCGTAATR motifs/meme_zoops/meme.txt  /work/data/genomes/fly/Drosophila_melanogaster/Ensembl/BDGP6/Sequence/WholeGenomeFasta/genome.fa
 
 # HOMER
-#findMotifsGenome.pl seqs/downPNA.bed dm6 motifs/homer -preparsedDir seqs
-#scanMotifGenomeWide.pl motifs/homer/homerResults/motif1.motif dm6 -bed > hits/homer.sites.dm6.bed
+findMotifsGenome.pl seqs/downPNA.bed dm6 motifs/homer -preparsedDir seqs
+scanMotifGenomeWide.pl motifs/homer/homerResults/motif1.motif dm6 -bed > hits/homer.sites.dm6.bed
 
